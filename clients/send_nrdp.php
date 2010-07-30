d8e694204d66f44065cdfe767f50a4489515c797
Ethan Galstad <egalstad@nagios.com>
2010-07-30 18:21:29 +0000

    License change
    git-svn-id: https://devhub.nagios.com/svn/nrdp@15 7ae7bfbb-3f01-4008-b2e3-ed148782e3a1

--- clients/send_nrdp.php
+++ clients/send_nrdp.php
@@ -4,8 +4,16 @@
 //
 // Copyright (c) 2010 Nagios Enterprises, LLC.
 // Portions Copyright (c) others - see source code below.
-// License: Nagios Open Software License <http://www.nagios.com/legal/licenses>
-//  
+/* License:
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+    * Neither the name of the Nagios Enterprises, LLC nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/  
 
 doit();
 
@@ -111,7 +119,7 @@ function check_args($args){
 		echo "send_nrdp - NRDP Host and Service Check Client\n";
 		echo "Copyright (c) 2010 Nagios Enterprises, LLC\n";
 		echo "Portions Copyright (c) others - see source code\n";
-		echo "License: Nagios Open Software License <http://www.nagios.com/legal/licenses>\n";
+		echo "License: BSD\n";
 		echo "\n";
 		echo "Usage: ".$argv[0]." --url=<url> --token=<token> --host=<hostname> [--service=<servicename>] --state=<state> --output=<output>\n";
 		echo "\n";
