754dc4f95043f52e7a9a0698505f74688bc39127
Scott Wilkerson <swilkerson@nagios.com>
2012-02-09 22:48:05 +0000

    adding 2 new send_nrpe clients, python and bash. Also, added beginning NRDS files. - SW
    git-svn-id: https://devhub.nagios.com/svn/nrdp@24 7ae7bfbb-3f01-4008-b2e3-ed148782e3a1

---
+++ clients/nrds/nrds.sh
@@ -0,0 +1,102 @@
+#!/bin/bash -e
+#
+# Copyright (c) 2010-2011 Nagios Enterprises, LLC.
+# 
+#
+###########################
+CONFIG=./nrds.cfg
+
+PROGNAME=$(basename $0)
+RELEASE="Revision 0.1"
+
+# Functions plugin usage
+print_release() {
+    echo "$RELEASE"
+}
+print_usage() {
+	echo ""
+	echo "$PROGNAME $RELEASE - Sends passive checks to Nagios NRPD server"
+	echo ""
+	echo "Usage: nrds.sh -H hostname [-u /path/to/nrds.cfg]"
+	echo ""
+    echo "Usage: $PROGNAME -h"
+    echo ""
+}
+
+print_help() {
+		print_usage
+        echo ""
+        echo "This script is used to send passive checks in nrds.cfg"
+		echo "to Nagios NRPD server"
+		echo ""
+		echo "Options:"
+		echo "	Single Check:"
+		echo "		-h	host name"
+		echo "		-s	service name"
+		echo "		-S	State"
+		echo "		-o 	output"
+		exit 0
+}
+
+in_array() {
+    local hay=$2 needle=$1
+    shift
+    for hay; do
+        [[ $hay == $needle ]] && return 0
+    done
+    return 1
+}
+
+while getopts "c:H:hv" option
+do
+  case $option in
+    c) CONFIG=$OPTARG ;;
+	H) hostname=$OPTARG ;;
+	h) print_help ;;
+	v) print_release
+		exit 0 ;;
+  esac
+done
+if [ ! $hostname ]; then
+ print_usage
+ exit 1
+fi
+
+
+
+
+# Process the config
+valid_fields=(URL TOKEN TMPDIR SEND_NRDP)
+i=0
+while read line; do
+if [[ "$line" =~ ^[^\#\;]*= ]]; then
+	#grab all the commands
+	if [ ${line:0:7} == "command" ];then
+		name[i]=`echo $line | cut -d'=' -f 1`
+		service[i]=${name[i]:8:(${#name[i]}-8-1)}
+		value[i]=`echo $line | cut -d'=' -f 2-`
+		((i++))
+	else
+	# not a command lets process the rest of the config
+	# first make sure it is part of valid_fields
+		if [[ "${valid_fields[@]}" =~ `echo $line | cut -d'=' -f 1` ]];then
+			eval $line
+		else	
+			echo "ERROR: `echo $line | cut -d'=' -f 1` is not a valid cfg field."
+			exit 1
+		fi
+	fi
+fi
+done < $CONFIG
+tmp=`mktemp $TMPDIR/output.XXXXXX`
+for (( i=0; i<=$(( ${#service[*]} -1 )); i++ ))
+do
+	(eval ${value[$i]})>$tmp
+	status="$?"
+	output=`cat $tmp`
+
+	echo -e "$hostname\t${service[$i]}\t$status\t$output" | $SEND_NRDP -u $URL -t $TOKEN
+	
+done
+rm -f $tmp
+
---
+++ clients/send_nrdp.sh
@@ -0,0 +1,227 @@
+#!/bin/bash -e
+#
+# Copyright (c) 2010-2011 Nagios Enterprises, LLC.
+# 
+#
+###########################
+
+PROGNAME=$(basename $0)
+RELEASE="Revision 0.1"
+
+print_release() {
+    echo "$RELEASE"
+}
+print_usage() {
+	echo ""
+	echo "$PROGNAME $RELEASE - Send NRPD script for Nagios"
+	echo ""
+	echo "Usage: send_nrdp.sh -u URL -t token [options]"
+	echo ""
+    echo "Usage: $PROGNAME -h"
+    echo ""
+}
+
+print_help() {
+		print_usage
+        echo ""
+        echo "This script is used to send NRPD data to a Nagios server"
+		echo ""
+        echo "Required:"
+		echo "	-u","	URL of NRDP server.  Usually http://<IP_ADDRESS>/nrdp/"
+		echo "	-t","	Shared token.  Must be the same token set in NRDP Server"
+		echo ""
+		echo "Options:"
+		echo "	Single Check:"
+		echo "		-h	host name"
+		echo "		-s	service name"
+		echo "		-S	State"
+		echo "		-o 	output"
+		echo ""
+		echo "	STDIN:"
+		echo "		-d	delimiter (default \"\\t\")"
+		echo "		With only the required parameters $PROGNAME is capable of"
+		echo "		processing data piped to it either from a file or other"
+		echo "		process.  By default, we use \t as the delimiter however this"
+		echo "		may be specified with the -d option data should be in the"
+		echo "		following formats one entry per line."
+		echo "		For Host checks:"
+		echo "		hostname	State	output"
+		echo "		For Service checks"
+		echo "		hostname	servicename	State	output"
+		echo ""
+		echo "	File:"
+		echo "		-f /full/path/to/file"
+		echo "		This file will be sent to the NRDP server specified in -u"
+		echo "		The file should be an XML file in the following format"
+		echo "		##################################################"
+		echo ""
+		echo "		<?xml version='1.0'?>"
+		echo "		<checkresults>"
+		echo "		  <checkresult type=\"host\" checktype=\"1\">"
+		echo "		    <hostname>YOUR_HOSTNAME</hostname>"
+		echo "		    <state>0</state>"
+		echo "		    <output>OK|perfdata=1.00;5;10;0</output>"
+		echo "		  </checkresult>"
+		echo "		  <checkresult type=\"service\" checktype=\"1\">"
+		echo "		    <hostname>YOUR_HOSTNAME</hostname>"
+		echo "		    <servicename>YOUR_SERVICENAME</servicename>"
+		echo "		    <state>0</state>"
+		echo "		    <output>OK|perfdata=1.00;5;10;0</output>"
+		echo "		  </checkresult>"
+		echo "		</checkresults>"
+		echo "		##################################################"
+		echo ""
+		echo "	Directory:"
+		echo "		-D /path/to/temp/dir"
+		echo "		This is a directory that contains XML files in the format"
+		echo "		above.  Additionally, if the -d flag is specified, $PROGNAME"
+		echo "		will create temp files here if the server could not be reached."
+		echo "		On additional calls with the same -D path, if a connection to"
+		echo "		the server is successful, all temp files will be sent."
+		exit 0
+}
+
+send_data() {
+	pdata="token=$token&cmd=submitcheck&XMLDATA=$1"
+	if [ $curl ];then
+		rslt=`curl --write-out %{http_code} --silent --output /dev/null -d "$pdata" $url`
+		#rslt=`curl -d "$pdata" $url`
+		#echo $rslt
+		if [ $rslt != 200 ];then
+			# This means we couldn't connect to NRPD server
+			# verify we are not processing the directory already and the directory
+			if [ ! "$2" ] && [ $directory ];then
+				# This is where we write to the tmp directory
+				echo $xml > `mktemp $directory/nrdp.XXXXXX`
+			fi
+			exit $rslt
+		fi
+		ret=$?
+	else
+		rslt=`wget -S -qO /dev/null --post-data="$pdata" $url`
+		ret=$?
+	fi
+	if [ "$2" ] && [ $ret == 0 ];then
+		rm -f "$2"
+	fi
+	if [ $ret != 0 ];then
+		echo "exited with error "$ret
+		exit $ret
+	fi
+}
+
+# Parse parameters
+
+while getopts "u:t:H:s:S:o:f:d:c:D:hv" option
+do
+  case $option in
+    u) url=$OPTARG ;;
+    t) token=$OPTARG ;;
+	H) host=$OPTARG ;;
+	s) service=$OPTARG ;;
+	S) State=$OPTARG ;;
+	o) output=$OPTARG ;;
+	f) file=$OPTARG ;;
+	d) delim=$OPTARG ;;
+	c) checktype=$OPTARG ;;
+	D) directory=$OPTARG ;;
+	h) print_help 0;;
+	v) print_release
+		exit 0 ;;
+  esac
+done
+
+if [ ! $checktype ]; then
+ checktype=1
+fi
+if [ ! $delim ]; then
+ delim=`echo -e "\t"`
+fi
+
+if [ "x$url" == "x" -o "x$token" == "x" ]
+then
+  echo "Usage: send_nrdp -u url -t token"
+  exit 1
+fi
+# detecting curl 
+if which curl > /dev/null; 
+ then curl=1; 
+fi
+# detecting wget if we don't have curl
+if [ ! $curl ] && [ which wget > /dev/null ];
+  then wget=1;
+fi
+
+if [[ ! $curl && ! $wget ]];
+then
+  echo "Either curl or wget are required to run this script"
+  exit 1
+fi
+
+if [ $host ]; then
+	xml=""
+	# we are not getting piped results
+	if [ ! $host ] || [ ! $State ]; then
+		echo "You must provide a host -H and State -S"
+		exit 2
+	fi
+	if [ $service ]; then
+		xml=$xml"<checkresult type='service' checktype='"$checktype"'>"
+		xml=$xml"<servicename>"$service"</servicename>"
+	else
+		xml=$xml"<checkresult type='host' checktype='"$checktype"'>"
+	fi
+	xml=$xml"<hostname>"$host"</hostname>"
+	xml=$xml"<state>"$State"</state>"
+	xml=$xml"<output>"$output"</output>"
+	xml=$xml"</checkresult>"
+fi
+# Detect STDIN
+########################
+if [ ! -t 0 ]; then
+	xml=""
+    # we know we are being piped results
+	IFS=$delim
+	while read -r line ; do
+		arr=($line)
+		if [ ${#arr[@]} != 0 ];then
+			if [[ ${#arr[@]} < 3 ]] || [[ ${#arr[@]} > 4 ]];then
+				echo "ERROR: STDIN must be either 3 or 4 fields long, I found "${#arr[@]}
+			else
+				if [ ${#arr[@]} == 4 ]; then
+					xml=$xml"<checkresult type='service' checktype='"$checktype"'>"
+					xml=$xml"<servicename>"${arr[1]}"</servicename>"
+				else
+					xml=$xml"<checkresult type='host' checktype='"$checktype"'>"
+				fi
+				xml=$xml"<hostname>"${arr[0]}"</hostname>"
+				xml=$xml"<state>"${arr[2]}"</state>"
+				xml=$xml"<output>"${arr[3]}"</output>"
+				xml=$xml"</checkresult>"
+			fi
+		fi
+    done
+	IFS=" "
+fi
+if [ $host ] || [ ! -t 0 ] ;then
+	xml="<?xml version='1.0'?><checkresults>"$xml"</checkresults>"
+	send_data "$xml"
+fi
+if [ $file ];then
+	xml=`cat $file`
+	send_data "$xml"
+fi
+if [ $directory ];then
+	#echo "Processing directory..."
+	for f in `ls $directory`
+	do
+	  #echo "Processing $f file..."
+	  # take action on each file. $f store current file name
+	  xml=`cat $directory/$f`
+	  #echo $xml
+	  send_data "$xml" "$directory/$f"
+	done
+	
+fi
+
+
