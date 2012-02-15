0de2347fc4aae2a5ebc3729b40f5a40d5a2c98be
Scott Wilkerson <swilkerson@nagios.com>
2012-02-15 16:52:07 +0000

    Had to get all the changes in while they work before I break something... Added NRDS config update server plugin. Added nrds_updater which can pull config and plugins from nrds server Starting to come together-SW
    git-svn-id: https://devhub.nagios.com/svn/nrdp@29 7ae7bfbb-3f01-4008-b2e3-ed148782e3a1

---
+++ clients/nrds/nrds_updater.sh
@@ -0,0 +1,131 @@
+#!/bin/bash -e
+#
+# Copyright (c) 2010-2011 Nagios Enterprises, LLC.
+# 
+#
+###########################
+
+PROGNAME=$(basename $0)
+RELEASE="Revision 0.1"
+
+# Functions plugin usage
+print_release() {
+    echo "$RELEASE"
+}
+print_usage() {
+	echo ""
+	echo "$PROGNAME $RELEASE - NRDS Updater from Nagios NRPD server"
+	echo ""
+	echo "Usage: nrds.sh -c /path/to/nrds.cfg"
+	echo ""
+    echo "Usage: $PROGNAME -h"
+    echo ""
+}
+
+print_help() {
+		print_usage
+        echo ""
+        echo "This script is used to update nrds.cfg"
+		echo "from Nagios NRPD server"
+		echo ""
+		exit 0
+}
+process_config(){
+# Process the config
+valid_fields=(URL TOKEN TMPDIR SEND_NRDP COMMAND_PREFIX CONFIG_NAME CONFIG_VERSION CONFIG_NAME UPDATE_CONFIG UPDATE_PLUGINS PLUGIN_DIR)
+i=0
+while read line; do
+if [[ "$line" =~ ^[^\#\;]*= ]]; then
+	#grab all the commands and put in arrays
+	if [ ${line:0:7} == "command" ];then
+		name[i]=`echo $line | cut -d'=' -f 1`
+		service[i]=${name[i]:8:(${#name[i]}-8-1)}
+		value[i]=`echo $line | cut -d'=' -f 2-`
+		((i++))
+	else
+	# not a command lets process the rest of the config
+	# first make sure it is part of valid_fields
+		if [[ "${valid_fields[@]}" =~ `echo $line | cut -d'=' -f 1` ]];then
+			eval $line
+		else	
+			echo "ERROR: `echo $line | cut -d'=' -f 1` is not a valid cfg field."
+			exit 1
+		fi
+	fi
+fi
+done < $CONFIG
+}
+send_data() {
+	pdata=$1
+	if [ $curl ];then
+		rslt=`curl --silent -d "$pdata" $URL`
+		ret=$?
+		status=`echo $rslt | sed -n 's|.*<status>\(.*\)</status>.*|\1|p'`
+		if [ $ret == 0 ] && [ $status == 1 ];then
+			save_config=`curl -o $CONFIG --silent -d "token=$TOKEN&cmd=getconfig&configname=$CONFIG_NAME" $URL`
+			process_config
+			# check if we need to update plugins
+			if [ "$UPDATE_PLUGINS" == "1" ];then
+				#this is where the plugin updates go
+				for (( i=0; i<=$(( ${#service[*]} -1 )); i++ ))
+				do
+					full_plugin_path=(${value[$i]})
+					plugin_name=`basename $full_plugin_path`
+					(`curl -o ${full_plugin_path} --silent -d "token=$TOKEN&cmd=getplugin&plugin=$plugin_name" $URL`)
+				done
+			fi
+		fi
+	else
+		rslt=`wget -S -qO /dev/null --post-data="$pdata" $URL`
+		ret=$?
+	fi
+	# If this was a directory call and was successful, remove the file
+	if [ "$2" ] && [ $ret == 0 ];then
+		rm -f "$2"
+	fi
+	# If we weren't successful error
+	if [ $ret != 0 ];then
+		echo "exited with error "$ret
+		exit $ret
+	fi
+}
+
+while getopts "c:hv" option
+do
+  case $option in
+	c) CONFIG=$OPTARG ;;
+	h) print_help 0;;
+	v) print_release
+		exit 0 ;;
+  esac
+done
+
+if [ ! -f $CONFIG ]; then
+ echo "Could not find config file at "$CONFIG
+ exit 1
+fi
+
+# detecting curl 
+if which curl > /dev/null; 
+ then curl=1; 
+fi
+# detecting wget if we don't have curl
+if [ ! $curl ] && [ which wget > /dev/null ];
+  then wget=1;
+fi
+
+if [[ ! $curl && ! $wget ]];
+then
+  echo "Either curl or wget are required to run this script"
+  exit 1
+fi
+
+process_config
+
+if [ "$UPDATE_CONFIG" == "1" ] && [ ! "x$CONFIG_NAME" == "x" ] && [ ! "x$CONFIG_VERSION" == "x" ];then
+	xml="<?xml version='1.0' ?><configs><config><name>$CONFIG_NAME</name><version>$CONFIG_VERSION</version></config></configs>"
+	send_data "token=$TOKEN&cmd=updatenrds&XMLDATA=$xml"
+fi
+	
+
+
