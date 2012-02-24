6a4276e1972f38f0be0e425fbb7236a3a4eb1abd
Scott Wilkerson <swilkerson@nagios.com>
2012-02-24 20:18:59 +0000

    Fixed whitespace problem -Alex
    git-svn-id: https://devhub.nagios.com/svn/nrdp@35 7ae7bfbb-3f01-4008-b2e3-ed148782e3a1

--- clients/nrds/nrds.sh
+++ clients/nrds/nrds.sh
@@ -17,83 +17,83 @@ print_release() {
     echo "$RELEASE"
 }
 print_usage() {
-	echo ""
-	echo "$PROGNAME $RELEASE - Sends passive checks to Nagios NRPD server"
-	echo ""
-	echo "Usage: nrds.sh -H hostname [-c /path/to/nrds.cfg]"
-	echo ""
+    echo ""
+    echo "$PROGNAME $RELEASE - Sends passive checks to Nagios NRPD server"
+    echo ""
+    echo "Usage: nrds.sh -H hostname [-c /path/to/nrds.cfg]"
+    echo ""
     echo "Usage: $PROGNAME -h display help"
     echo ""
 }
 
 print_help() {
-		print_usage
-        echo ""
-        echo "This script is used to send passive checks in nrds.cfg"
-		echo "to Nagios NRPD server"
-		echo ""
-		exit 0
+    print_usage
+    echo ""
+    echo "This script is used to send passive checks in nrds.cfg"
+    echo "to Nagios NRPD server"
+    echo ""
+    exit 0
 }
 
 while getopts "c:H:hv" option
 do
-  case $option in
-    c) CONFIG=$OPTARG ;;
-	H) hostname=$OPTARG ;;
-	h) print_help ;;
-	v) print_release
-		exit 0 ;;
-  esac
+    case $option in
+        c) CONFIG=$OPTARG ;;
+        H) hostname=$OPTARG ;;
+        h) print_help ;;
+        v) print_release
+            exit 0 ;;
+    esac
 done
 if [ ! $hostname ]; then
- print_usage
- exit 1
+    print_usage
+    exit 1
 fi
 if [ ! -f $CONFIG ];then
-	echo "Could not find config file at "$CONFIG
-	exit 1
+    echo "Could not find config file at "$CONFIG
+    exit 1
 fi
 
 # Process the config
 valid_fields=(URL TOKEN TMPDIR SEND_NRDP COMMAND_PREFIX CONFIG_NAME CONFIG_VERSION CONFIG_NAME UPDATE_CONFIG UPDATE_PLUGINS PLUGIN_DIR)
 i=0
 while read line; do
-if [[ "$line" =~ ^[^\#\;]*= ]]; then
-	#grab all the commands
-	if [ ${line:0:7} == "command" ];then
-		name[i]=`echo $line | cut -d'=' -f 1`
-		service[i]=${name[i]:8:(${#name[i]}-8-1)}
-		value[i]=`echo $line | cut -d'=' -f 2-`
-		((i++))
-	else
-	# not a command lets process the rest of the config
-	# first make sure it is part of valid_fields
-		if [[ "${valid_fields[@]}" =~ `echo $line | cut -d'=' -f 1` ]];then
-			eval $line
-		else	
-			echo "ERROR: `echo $line | cut -d'=' -f 1` is not a valid cfg field."
-			exit 1
-		fi
-	fi
-fi
+    if [[ "$line" =~ ^[^\#\;]*= ]]; then
+        #grab all the commands
+        if [ ${line:0:7} == "command" ];then
+            name[i]=`echo $line | cut -d'=' -f 1`
+            service[i]=${name[i]:8:(${#name[i]}-8-1)}
+            value[i]=`echo $line | cut -d'=' -f 2-`
+            ((i++))
+        else
+            # not a command lets process the rest of the config
+            # first make sure it is part of valid_fields
+            if [[ "${valid_fields[@]}" =~ `echo $line | cut -d'=' -f 1` ]];then
+                eval $line
+            else	
+                echo "ERROR: `echo $line | cut -d'=' -f 1` is not a valid cfg field."
+                exit 1
+            fi
+        fi
+    fi
 done < $CONFIG
 if [ ! -f $SEND_NRDP ];then
-	echo "Could not find SEND_NRDP file at $SEND_NRDP"
-	exit 1
+    echo "Could not find SEND_NRDP file at $SEND_NRDP"
+    exit 1
 fi
 
 for (( i=0; i<=$(( ${#service[*]} -1 )); i++ ))
 do
-		output=$(eval "$COMMAND_PREFIX ${value[$i]}")
-		status="$?"
-	if [ "${service[$i]}" == "__HOST__" ];then
-		senddata="$senddata$hostname\t$status\t$output\n"
-	else
-		senddata="$senddata$hostname\t${service[$i]}\t$status\t$output\n"
-	fi
+    output=$(eval "$COMMAND_PREFIX ${value[$i]}")
+    status="$?"
+    if [ "${service[$i]}" == "__HOST__" ];then
+        senddata="$senddata$hostname\t$status\t$output\n"
+    else
+        senddata="$senddata$hostname\t${service[$i]}\t$status\t$output\n"
+    fi
 done
 if [ $TMPDIR ];then
-	mytmpdir="-d $TMPDIR"
+    mytmpdir="-d $TMPDIR"
 fi
 echo -e $senddata | $SEND_NRDP -u "$URL" -t "$TOKEN"
 
--- clients/nrds/nrds_updater.sh
+++ clients/nrds/nrds_updater.sh
@@ -13,153 +13,153 @@ print_release() {
     echo "$RELEASE"
 }
 print_usage() {
-	echo ""
-	echo "$PROGNAME $RELEASE - NRDS Updater from Nagios NRPD server"
-	echo ""
-	echo "Usage: nrds.sh -c /path/to/nrds.cfg"
-	echo ""
+    echo ""
+    echo "$PROGNAME $RELEASE - NRDS Updater from Nagios NRPD server"
+    echo ""
+    echo "Usage: nrds.sh -c /path/to/nrds.cfg"
+    echo ""
     echo "Usage: $PROGNAME -h"
     echo ""
 }
 print_help() {
-		print_usage
-        echo ""
-        echo "This script is used to update nrds.cfg"
-		echo "from Nagios NRPD server"
-		echo ""
-		exit 0
+    print_usage
+    echo ""
+    echo "This script is used to update nrds.cfg"
+    echo "from Nagios NRPD server"
+    echo ""
+    exit 0
 }
 process_config(){
-# Process the config
-valid_fields=(URL TOKEN TMPDIR SEND_NRDP COMMAND_PREFIX CONFIG_NAME CONFIG_VERSION CONFIG_NAME UPDATE_CONFIG UPDATE_PLUGINS PLUGIN_DIR)
-i=0
-while read line; do
-if [[ "$line" =~ ^[^\#\;]*= ]]; then
-	#grab all the commands and put in arrays
-	if [ ${line:0:7} == "command" ];then
-		name[i]=`echo $line | cut -d'=' -f 1`
-		service[i]=${name[i]:8:(${#name[i]}-8-1)}
-		value[i]=`echo $line | cut -d'=' -f 2-`
-		((i++))
-	else
-	# not a command lets process the rest of the config
-	# first make sure it is part of valid_fields
-		if [[ "${valid_fields[@]}" =~ `echo $line | cut -d'=' -f 1` ]];then
-			eval $line
-		else	
-			echo "ERROR: `echo $line | cut -d'=' -f 1` is not a valid cfg field."
-			exit 1
-		fi
-	fi
-fi
-done < $CONFIG
+    # Process the config
+    valid_fields=(URL TOKEN TMPDIR SEND_NRDP COMMAND_PREFIX CONFIG_NAME CONFIG_VERSION CONFIG_NAME UPDATE_CONFIG UPDATE_PLUGINS PLUGIN_DIR)
+    i=0
+    while read line; do
+        if [[ "$line" =~ ^[^\#\;]*= ]]; then
+            #grab all the commands and put in arrays
+            if [ ${line:0:7} == "command" ];then
+                name[i]=`echo $line | cut -d'=' -f 1`
+                service[i]=${name[i]:8:(${#name[i]}-8-1)}
+                value[i]=`echo $line | cut -d'=' -f 2-`
+                ((i++))
+            else
+                # not a command lets process the rest of the config
+                # first make sure it is part of valid_fields
+                if [[ "${valid_fields[@]}" =~ `echo $line | cut -d'=' -f 1` ]];then
+                    eval $line
+                else	
+                    echo "ERROR: `echo $line | cut -d'=' -f 1` is not a valid cfg field."
+                    exit 1
+                fi
+            fi
+        fi
+    done < $CONFIG
 }
 send_data() {
-	pdata=$1
-	if [ $curl ];then
-		rslt=`curl --silent -d "$pdata" $URL`
-	else
-		rslt=`wget -q -O - --post-data="$pdata" $URL`
-	fi
-	ret=$?
-	status=`echo $rslt | sed -n 's|.*<status>\(.*\)</status>.*|\1|p'`
-	message=`echo $rslt | sed -n 's|.*<message>\(.*\)</message>.*|\1|p'`
-	if [[ $rslt =~ "NO REQUEST HANDLER" ]];then
-		echo "ERROR $rslt. Check the server config and version" 
-		exit 1
-	fi
-	if [ ! $status ];then
-		echo "ERROR: Could not connect to $URL check your cfg file."
-		exit 1
-	fi
-	if [ "$status" == "-1" ];then
-		echo "ERROR: NRDP Server said - $message"
-		exit 1
-	fi
-	if [ "$status" == "1" ];then
-		if [ $curl ];then
-			save_config=`curl -o $CONFIG --silent -d "token=$TOKEN&cmd=getconfig&configname=$CONFIG_NAME" $URL`
-		else
-			save_config=`wget -qO $CONFIG --post-data="token=$TOKEN&cmd=getconfig&configname=$CONFIG_NAME" $URL`
-		fi
-		process_config
-		echo "Updated config to version $CONFIG_VERSION"
-		# check if we need to update plugins
-		if [ "$UPDATE_PLUGINS" == "1" ];then
-			for (( i=0; i<=$(( ${#service[*]} -1 )); i++ ))
-			do
-				full_plugin_path=(${value[$i]})
-				plugin_name=`basename $full_plugin_path`
-				# this if makes sure we aren't downloading the same plugin twice
-				if [[ ! "${unique_plugins[@]}" =~ "${plugin_name}" ]];then
-					#make dir if it doesn't exist
-					DIRNAME=`dirname $full_plugin_path`
-					if [ ! -d "$DIRNAME" ];then
-						mkdir -p "$DIRNAME"
-						chown nagios.nagios "$DIRNAME"
-					fi
-					if [ $curl ];then
-						`curl -o ${full_plugin_path} --silent -d "token=$TOKEN&cmd=getplugin&plugin=$plugin_name" $URL`
-					else
-						`wget -qO ${full_plugin_path} --post-data="token=$TOKEN&cmd=getplugin&plugin=$plugin_name" $URL`
-					fi
-					# add permission changes here ?
-					chmod +x "${full_plugin_path}"
-					if [ "${plugin_name}" == "check_icmp" -o "${plugin_name}" == "check_dhcp" ];then
-						chmod u+s "${full_plugin_path}"
-					fi
-					unique_plugins+=("${plugin_name}")
-				fi
-			done
-			echo "Updated ${#unique_plugins[*]} plugins"
-		fi
-	fi
-	# If we weren't successful error
-	if [ $ret != 0 ];then
-		echo "exited with error "$ret
-		exit $ret
-	fi
+    pdata=$1
+    if [ $curl ];then
+        rslt=`curl --silent -d "$pdata" $URL`
+    else
+        rslt=`wget -q -O - --post-data="$pdata" $URL`
+    fi
+    ret=$?
+    status=`echo $rslt | sed -n 's|.*<status>\(.*\)</status>.*|\1|p'`
+    message=`echo $rslt | sed -n 's|.*<message>\(.*\)</message>.*|\1|p'`
+    if [[ $rslt =~ "NO REQUEST HANDLER" ]];then
+        echo "ERROR $rslt. Check the server config and version" 
+        exit 1
+    fi
+    if [ ! $status ];then
+        echo "ERROR: Could not connect to $URL check your cfg file."
+        exit 1
+    fi
+    if [ "$status" == "-1" ];then
+        echo "ERROR: NRDP Server said - $message"
+        exit 1
+    fi
+    if [ "$status" == "1" ];then
+        if [ $curl ];then
+            save_config=`curl -o $CONFIG --silent -d "token=$TOKEN&cmd=getconfig&configname=$CONFIG_NAME" $URL`
+        else
+            save_config=`wget -qO $CONFIG --post-data="token=$TOKEN&cmd=getconfig&configname=$CONFIG_NAME" $URL`
+        fi
+        process_config
+        echo "Updated config to version $CONFIG_VERSION"
+        # check if we need to update plugins
+        if [ "$UPDATE_PLUGINS" == "1" ];then
+            for (( i=0; i<=$(( ${#service[*]} -1 )); i++ ))
+            do
+                full_plugin_path=(${value[$i]})
+                plugin_name=`basename $full_plugin_path`
+                # this if makes sure we aren't downloading the same plugin twice
+                if [[ ! "${unique_plugins[@]}" =~ "${plugin_name}" ]];then
+                    #make dir if it doesn't exist
+                    DIRNAME=`dirname $full_plugin_path`
+                    if [ ! -d "$DIRNAME" ];then
+                        mkdir -p "$DIRNAME"
+                        chown nagios.nagios "$DIRNAME"
+                    fi
+                    if [ $curl ];then
+                        `curl -o ${full_plugin_path} --silent -d "token=$TOKEN&cmd=getplugin&plugin=$plugin_name" $URL`
+                    else
+                        `wget -qO ${full_plugin_path} --post-data="token=$TOKEN&cmd=getplugin&plugin=$plugin_name" $URL`
+                    fi
+                    # add permission changes here ?
+                    chmod +x "${full_plugin_path}"
+                    if [ "${plugin_name}" == "check_icmp" -o "${plugin_name}" == "check_dhcp" ];then
+                        chmod u+s "${full_plugin_path}"
+                    fi
+                        unique_plugins+=("${plugin_name}")
+                fi
+            done
+            echo "Updated ${#unique_plugins[*]} plugins"
+        fi
+    fi
+    # If we weren't successful error
+    if [ $ret != 0 ];then
+        echo "exited with error "$ret
+        exit $ret
+    fi
 }
 
 while getopts "c:hv" option
 do
-  case $option in
-	c) CONFIG=$OPTARG ;;
-	h) print_help 0;;
-	v) print_release
-		exit 0 ;;
-  esac
+    case $option in
+        c) CONFIG=$OPTARG ;;
+        h) print_help 0;;
+        v) print_release
+            exit 0 ;;
+    esac
 done
 
 if [ ! -f $CONFIG ]; then
- echo "Could not find config file at "$CONFIG
- exit 1
+    echo "Could not find config file at "$CONFIG
+    exit 1
 fi
 
 # detecting curl 
-if which curl > /dev/null; 
- then curl=1; 
+if which curl > /dev/null
+then
+    curl=1; 
 fi
 # detecting wget if we don't have curl
-if [ ! $curl ] && [ which wget > /dev/null ];
-  then wget=1;
+if [ ! $curl ] && [ which wget > /dev/null ]
+then
+    wget=1;
 fi
 
-if [[ ! $curl && ! $wget ]];
+if [[ ! $curl && ! $wget ]]
 then
-  echo "Either curl or wget are required to run this script"
-  exit 1
+    echo "Either curl or wget are required to run this script"
+    exit 1
 fi
 
 process_config
 if [[ "${URL}" =~ "localhost" ]];then
-	echo "ERROR: This should not be run on the localhost"
-exit 1
+    echo "ERROR: This should not be run on the localhost"
+    exit 1
 fi
 if [ "$UPDATE_CONFIG" == "1" ] && [ ! "x$CONFIG_NAME" == "x" ] && [ ! "x$CONFIG_VERSION" == "x" ];then
-	xml="<?xml version='1.0' ?><configs><config><name>$CONFIG_NAME</name><version>$CONFIG_VERSION</version></config></configs>"
-	send_data "token=$TOKEN&cmd=updatenrds&XMLDATA=$xml"
+    xml="<?xml version='1.0' ?><configs><config><name>$CONFIG_NAME</name><version>$CONFIG_VERSION</version></config></configs>"
+    send_data "token=$TOKEN&cmd=updatenrds&XMLDATA=$xml"
 fi
-	
-
 
